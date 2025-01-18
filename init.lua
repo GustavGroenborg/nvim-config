6615bc11365252e8017b4205be22e51b7de2f24d
Gustav C. R. Groenborg <gustavrisagerus@gmail.com>
2025-01-18 14:43:56 +0100

    Replaced init.vim with init.lua

---
+++ init.lua
@@ -0,0 +1,4 @@
+vim.g.python3_host_prog = "/home/gcrg/.pyenv/versions/neovim/bin/python"
+require("config")
+require("lspconfig")
+require("plugins")
---
+++ lua/config/init.lua
@@ -0,0 +1,4 @@
+require("config.lazy")
+require("config.custom-commands")
+require("config.general-settings")
+require("config.style-settings")
