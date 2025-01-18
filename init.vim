267f9ed74d2006337098e23ebf366b0919bb40ce
Gustav C. R. Groenborg <gustavrisagerus@gmail.com>
2025-01-18 12:46:22 +0100

    Ported style settings from vimscript

--- init.vim
+++ init.vim
@@ -4,6 +4,7 @@ source ~/.nvimrc
 
 let g:python3_host_prog = '/home/gcrg/.pyenv/versions/neovim/bin/python'
 lua require('config.lazy')
+lua require('config.style-settings')
 lua require('lspconfig')
 lua require('plugins')
 "lua require('after/ftplugin/haskell')
---
+++ lua/config/style-settings.lua
@@ -0,0 +1,42 @@
+local style_group = vim.api.nvim_create_augroup("stylesettings", { clear = true })
+local file_patterns = { "markdown", "tex", "cpp", "java", "rust" }
+
+-- Helper functions
+local function setup_filetype_width(file_pattern, augroup)
+    vim.api.nvim_create_autocmd(
+        { "FileType" },
+        {
+            pattern = file_pattern,
+            group   = augroup,
+            command = "setlocal textwidth=80",
+        }
+    )
+end
+
+local function setup_filetype_spell(file_pattern, augroup)
+    vim.api.nvim_create_autocmd(
+        { "FileType" },
+        {
+            pattern = file_pattern,
+            group   = augroup,
+            command  = "setlocal spell spelllang=en_gb",
+        }
+    )
+end
+
+
+-- Basic setup
+for _, file_pattern in ipairs(file_patterns) do
+    setup_filetype_width(file_pattern, style_group)
+    setup_filetype_spell(file_pattern, style_group)
+end
+
+-- Setting syntax for omz themes
+vim.api.nvim_create_autocmd(
+    { "BufRead", "BufNewFile" },
+    {
+        pattern = "*.zsh-theme",
+        group   = style_group,
+        command = "set syntax=zsh",
+    }
+)
